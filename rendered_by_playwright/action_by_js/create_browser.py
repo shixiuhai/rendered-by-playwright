7a04e608f4ca5b4bcfc4f0d66dc6c804f4ffaef2
shixiuhai <15256728901@163.com>
2023-12-05 20:22:53 +0800

    更新了浏览器对象创建描述

--- rendered_by_playwright/action_by_js/create_browser.py
+++ rendered_by_playwright/action_by_js/create_browser.py
@@ -2,6 +2,7 @@ from playwright.async_api import async_playwright
 from rendered_by_playwright.settings import HEADLESS
 import platform
 import asyncio
+from rendered_by_playwright.utils.log import rendered_logger
 os_name = platform.system()
 if os_name == "Windows":
     HEADLESS = False
@@ -14,18 +15,25 @@ class Browser:
     async def create(self):
             if Browser.chromium is None:
                 Browser.chromium = await self.create_chromium_browser()
+                rendered_logger.info("chromium浏览器不存在,创建了chromium浏览器对象")
             if not Browser.chromium.is_connected():
                 Browser.chromium = await self.create_chromium_browser()
-            
+                rendered_logger.info("chromium浏览器被销毁,创建了新的chromium浏览器对象")
+                
             if Browser.firefox is None:
                 Browser.firefox = await self.create_firefox_browser()
+                rendered_logger.info("firefox浏览器不存在,创建了firefox浏览器对象")
             if not Browser.firefox.is_connected():
                 Browser.firefox = await self.create_firefox_browser()
+                rendered_logger.info("firefox浏览器被销毁,创建了新的firefox浏览器对象")
             
             if Browser.webkit is None:
                 Browser.webkit = await self.create_webkit_browser()
+                rendered_logger.info("webkit浏览器不存在,创建了webkit浏览器对象")
             if not Browser.webkit.is_connected():
                 Browser.webkit = await self.create_webkit_browser()
+                rendered_logger.info("webkit浏览器被销毁,创建了新的webkit浏览器对象")
+                
         
     async def create_chromium_browser(self):
         p = await async_playwright().start()  # 初始化 Playwright
