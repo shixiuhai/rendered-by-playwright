f7e250997b111e2e7e3085474192b0fb9d57d80d
shixiuhai <15256728901@163.com>
2023-12-06 00:31:50 +0800

    更新了页面滑动js

--- usually_js_script/滑动页面到最底端.js
+++ usually_js_script/滑动页面到最底端.js
@@ -1,23 +1,41 @@
 // 代码结构化数据，借用GPT实现的合并
 async () => {
     await new Promise((resolve) => {
-        const distance = 300;  // 每次滚动的距离
-        const delay = 200;  // 滚动之间的延迟时间
-
-        function scrollToBottom() {
-            const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-            const scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
-            if (scrollTop + window.innerHeight >= scrollHeight) {
-                resolve();
-            } else {
-                window.scrollBy(0, distance);
-                setTimeout(scrollToBottom, delay);
-            }
+      const getRandomValue = (min, max) => Math.random() * (max - min) + min;
+  
+      // 随机滑动距离和延迟
+      const distance = getRandomValue(200, 500);
+      const delay = getRandomValue(100, 300);
+  
+      // 超时时间
+      const timeout = 7000;
+  
+      // 记录开始时间
+      let startTime = Date.now();
+  
+      function scroll() {
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - startTime;
+  
+        // 获取滚动位置和页面高度
+        const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        const scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+  
+        // 判断是否滚动到底部或超时
+        if (scrollTop + window.innerHeight >= scrollHeight || elapsedTime >= timeout) {
+          resolve();
+        } else {
+          // 滚动并设置下一次滚动的延迟
+          window.scrollBy(0, distance);
+          setTimeout(scroll, delay);
         }
-
-        scrollToBottom();
+      }
+  
+      // 初始调用滚动函数
+      scroll();
     });
-}
+  };
+  
 
 // 代码合并成一行的数据, 请求复制这个字符串 有一定概率死循环，已经弃用
 //async () => { await new Promise((resolve) => { const distance = 300; const delay = 200; function scrollToBottom() { const scrollTop = document.documentElement.scrollTop || document.body.scrollTop; const scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight); if (scrollTop + window.innerHeight >= scrollHeight) { resolve(); } else { window.scrollBy(0, distance); setTimeout(scrollToBottom, delay); } } scrollToBottom(); }); }
