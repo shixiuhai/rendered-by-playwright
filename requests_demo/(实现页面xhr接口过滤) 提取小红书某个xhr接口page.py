b56323ce218aa67485db2cb20e929d7dc2210f43
shixiuhai <15256728901@163.com>
2023-12-13 20:37:55 +0800

    小红书信息提取测试

--- requests_demo/(实现页面xhr接口过滤) 提取小红书某个xhr接口page.py
+++ requests_demo/(实现页面xhr接口过滤) 提取小红书某个xhr接口page.py
@@ -3,12 +3,12 @@ import json
 url = "http://127.0.0.1:9001/rendered_by_playwright/requests"
 
 payload = {
-    "url": "https://www.xiaohongshu.com/explore/654ed925000000001b00c2bc",
+    "url": "https://www.xiaohongshu.com/explore/6562c575000000001b00dd80",
     "browser_type": "chromium",
-    "timeout": 8,
+    "timeout": 6,
     "return_type": "handle_xhr",
     "handle_xhr_path": "/api/sns/web/v2/comment/page",
-    "after_page_load_delay": 1
+    "after_page_load_delay": 2
 }
 headers = {"content-type": "application/json"}
 
@@ -18,6 +18,8 @@ if len(handle_xhr_response_json)>0:
     resp_text = handle_xhr_response_json[0]
     resp_text = resp_text.replace("true","True").replace("false","False") # 将接口json数据中的 true和false转换为python可以解释的True和False
     resp_json = eval(resp_text)
-    cursor=resp_json["data"]["cursor"]
+    # cursor=resp_json["data"]["cursor"] # 方法1
+    cursor=resp_json.get("data","").get("cursor","")# 方法2 方法2可以配置默认值
+    
     print(cursor)
     
