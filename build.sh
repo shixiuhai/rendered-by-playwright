ad959ad7592bbd9eb2f0fef20bd00fc413ea582e
shixiuhai <15256728901@163.com>
2023-12-13 23:14:19 +0800

    添加了在部署前如果容器存在先移除容器

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# docker stop rendered_by_playwright_project
-# docker rm  rendered_by_playwright_project
+docker stop rendered_by_playwright_project
+docker rm  rendered_by_playwright_project
 docker build -t rendered_by_playwright .
 docker run -itd --name rendered_by_playwright_project -p 9001:9001 rendered_by_playwright /bin/bash
