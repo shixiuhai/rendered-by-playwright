013254671e0720174c7dcb11cf668fcf0a38ea23
shixiuhai <15256728901@163.com>
2024-06-22 13:41:21 +0800

    删除不不用的代码

--- rendered_by_playwright/main.py
+++ rendered_by_playwright/main.py
@@ -3,11 +3,11 @@ sys.path.append("..")
 import uvicorn
 from rendered_by_playwright.utils.log import rendered_logger
 from rendered_by_playwright.settings import API_WORKERS
-import platform
-# 获取操作系统名称
-os_name = platform.system()
-if os_name == "Windows":
-    API_WORKERS = 1
+# import platform
+# # 获取操作系统名称
+# os_name = platform.system()
+# if os_name == "Windows":
+#     API_WORKERS = 1
 
 if __name__ == "__main__":
     rendered_logger.info("渲染框架启动")
