2bd2d96baed3d66adb5951f1823d50b91f746438
shixiuhai <15256728901@163.com>
2024-06-22 15:25:21 +0800

    更新了默认配置

--- rendered_by_playwright/settings.py
+++ rendered_by_playwright/settings.py
@@ -1,4 +1,4 @@
 LOG_LEVEL = 'INFO' # 日志级别
-HEADLESS = False # 是否开启无头模式
-API_WORKERS = 4 # 工作进程数
-CLOSED_WINDOWS=True # 启动后是否关闭窗口
+HEADLESS = True # 是否开启无头模式
+API_WORKERS = 4 # linux部署工作进程数
+CLOSED_WINDOWS=True # 开启窗口后是否触发自动关闭
